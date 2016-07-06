ee3aa42a006e366e5c5cbfc8473c39d5da956252
Farnoosh Johnson and Fernanda Lowe <farnoosh+fernanda@>
2016-07-06 13:29:44 -0700

    refactore by creating services, ui, and models packages

--- app/src/main/java/com/epicodus/movieapp/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/movieapp/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.epicodus.movieapp;
+package com.epicodus.movieapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.epicodus.movieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
